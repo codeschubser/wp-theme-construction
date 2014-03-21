bc5e63111e4b5a13f34e0388ad657f068bc78eb3
mitopp <mitopp@ROWS0089.avocis.local>
2014-03-21 11:32:25 +0100

    Add default content

--- header.php
+++ header.php
@@ -25,3 +25,15 @@
  *              IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  *              CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+    <head>
+        <meta charset="<?php bloginfo( 'charset' ); ?>" />
+        <title><?php wp_title( '-', true, 'right' ); ?></title>
+        <meta name="description" content="<?php bloginfo( 'description' ); ?>" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>">
+        <?php wp_head(); ?>
+    </head>
+    <body <?php body_class(); ?>>
